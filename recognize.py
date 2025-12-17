dcadbfe55f2dbf4b1e565ed6572dd88d1f0fa34e
ChrisVanBretus <davaijapan123@gmail.com>
2025-12-17 10:18:54 +0500

    Connecting the test dataset to recognize

--- recognize.py
+++ recognize.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import joblib
 import numpy as np
@@ -6,43 +7,40 @@ from utils import detect_face, preprocess, extract_hog
 model = joblib.load("face_model.pkl")
 labels = joblib.load("labels.pkl")
 
-image = cv2.imread("test.jpg")
-if image is None:
-    raise FileNotFoundError("Файл test.jpg не найден!")
+test_folder = "test_dataset"
 
-face, box = detect_face(image)
-if face is None or box is None:
-    print("Лицо не найдено")
-    exit()
+for img_name in os.listdir(test_folder):
+    img_path = os.path.join(test_folder, img_name)
+    image = cv2.imread(img_path)
+    if image is None:
+        print(f"Не удалось открыть файл {img_name}")
+        continue
 
-face = preprocess(face)
-features = extract_hog(face)
+    face, box = detect_face(image)
+    if face is None or box is None:
+        print(f"Лицо не найдено на {img_name}")
+        continue
 
-probs = model.predict_proba([features])[0]
-best_id = int(np.argmax(probs))
-confidence = float(probs[best_id] * 100)
-name = labels[best_id]
+    face = preprocess(face)
+    features = extract_hog(face)
 
-x, y, w, h = map(int, box)
+    probs = model.predict_proba([features])[0]
+    best_id = int(np.argmax(probs))
+    confidence = float(probs[best_id] * 100)
+    name = labels[best_id]
 
-if confidence >= 70:
-    text = f"Добро пожаловать, {name}! ({confidence:.1f}%)"
-    color = (0, 255, 0)
-else:
-    text = "Доступ запрещён"
-    color = (0, 0, 255)
+    x, y, w, h = map(int, box)
+    if confidence >= 70:
+        text = f"Добро пожаловать, {name}! ({confidence:.1f}%)"
+        color = (0, 255, 0)
+    else:
+        text = "Доступ запрещён"
+        color = (0, 0, 255)
 
-cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-cv2.putText(
-    image,
-    text,
-    (x, y - 10),
-    cv2.FONT_HERSHEY_SIMPLEX,
-    0.7,
-    color,
-    2
-)
+    cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+    cv2.putText(image, text, (x, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
 
-cv2.imshow("Face Access System", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow(f"Результат: {img_name}", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
